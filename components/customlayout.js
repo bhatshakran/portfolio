f05860d6f2702a96e0171265047a4393a21c140b
Bhat Shaqran <bhatshakran@gmail.com>
2022-06-28 20:52:23 +0530

    added callblind

---
+++ components/customlayout.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import styles from "./customlayout.module.css";
+import Link from "next/link";
+
+const name = "Bhat Shaqran";
+export const siteTitle = "Next.js Sample Website";
+
+export default function Layout(props) {
+  const { children, home } = props;
+  return (
+    <div className={styles.parent_container}>
+      <div className={styles.container}>
+        {/* <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Learn how to build a personal website using Next.js"
+          />
+          <meta
+            property="og:image"
+            content={`https://og-image.vercel.app/${encodeURI(
+              siteTitle
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head> */}
+
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
