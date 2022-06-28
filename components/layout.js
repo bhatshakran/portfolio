ba775a38892bd31daf39eeb07fb935e21bd1841a
Bhat Shaqran <bhatshakran@gmail.com>
2022-06-28 18:37:27 +0530

    Blog initiated

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,8 @@ import Link from "next/link";
 const name = "Bhat Shaqran";
 export const siteTitle = "Next.js Sample Website";
 
-export default function Layout({ children, home }) {
+export default function Layout(props) {
+  const { children, home } = props;
   return (
     <div className={styles.container}>
       <Head>
